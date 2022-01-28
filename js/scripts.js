25f1fccea75c39df0d2e486bb08ee7011c5a1be4
hmcvay <hayleymcvay8@gmail.com>
2022-01-28 11:26:11 -0800

    Add ptototype method to satisfy third spec test.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 //write cart constructor
 function Cart() {
   this.pizzasOrdered = {};
-  this.assignId = 0;
+  this.currentId = 0;
 }
 
 //write prototype method for adding pizzas to cart
@@ -11,6 +11,20 @@ Cart.prototype.addPizzas = function(pizza) {
   pizza.id = this.assignId();
   this.pizzasOrdered[pizza.id] = pizza;
 };
+
+//write prototype method for assigning ids to pizzas
+Cart.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+//test out adding to Cart//
+let myCart = new Cart ();
+let pizza1 = new Pizza ("pineapple", "large");
+let pizza2 = new Pizza ("pepperoni", "small");
+myCart.addPizzas(pizza1);
+myCart.addPizzas(pizza2);
+console.log(myCart);
 
 // write pizza object constructor
 function Pizza(toppings, size) {
@@ -21,8 +35,6 @@ function Pizza(toppings, size) {
 }
 
 // write prototype method for increasing price based on size
-
-
 
 Pizza.prototype.addSize = function () {
   if (this.size === "family") {
