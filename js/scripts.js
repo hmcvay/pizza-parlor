fd649a9e051bfd9afaca1f670178b8836f0a5a78
hmcvay <hayleymcvay8@gmail.com>
2022-01-28 12:42:26 -0800

    BACK FROM LUNCH.

--- js/scripts.js
+++ js/scripts.js
@@ -30,23 +30,23 @@ console.log(myCart);
 function Pizza(toppings, size) {
   this.toppings = toppings
   this.size = size
-  //this.basePrice = 7
-  this.price = 0
+  this.basePrice = 7
+  //this.price = 0
 }
 
 // write prototype method for increasing price based on size
 
 Pizza.prototype.addSize = function () {
+  let startPrice = this.basePrice
   if (this.size === "family") {
-    this.price = 16
+    startPrice += 9
   } else if (this.size === "large") {
-    this.price = 13
+    startPrice += 6
   } else if (this.size === "medium") {
-    this.price = 10
+    startPrice += 3
   } else {
     this.price = 7
   }
-  this.price = price
 };
 let pizza1 = new Pizza ("pineapple", "family");
 console.log(pizza1);
