266584278bc082cb82b24f777f64d6c8a822ca97
hmcvay <hayleymcvay8@gmail.com>
2022-02-06 13:23:43 -0800

    Add ptototype method to increase cost based on style.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 // BUSINESS LOGIC
 
-function Pizza(size, toppings) {
+function Pizza(size, toppings, style) {
   this.size = size;
   this.toppings = toppings;
+  this.style = style;
   this.price = 7;
 }
 
@@ -20,45 +21,57 @@ Pizza.prototype.sizeCost = function () {
 }
 
 Pizza.prototype.toppingCost = function () {
-    if (this.toppings.includes("ex. cheese")) {
-      this.price +=2;
-    };
-    if (this.toppings.includes("pepperoni")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes("salami")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes("sausage")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes("anchovies")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes("mushrooms")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes("olives")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes("garlic")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes ("peppers")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes("onions")) {
-      this.price +=1;
-    };
-    if (this.toppings.includes("tomatoes")) {
-      this.price +1;
-    };
-    return this.price;
-  }
+  if (this.toppings.includes("ex. cheese")) {
+    this.price +=2;
+  };
+  if (this.toppings.includes("pepperoni")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes("salami")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes("sausage")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes("anchovies")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes("mushrooms")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes("olives")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes("garlic")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes ("peppers")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes("onions")) {
+    this.price +=1;
+  };
+  if (this.toppings.includes("tomatoes")) {
+    this.price +1;
+  };
+  return this.price;
+}
+
+Pizza.prototype.styleCost = function () {
+  if (this.style === "regular") {
+    this.price += 0;
+  } else if (this.style === "thin") {
+    this.price += 0;
+  } else if (this.style === "deep dish") {
+    this.price += 2;
+  };
+  return this.price;
+}
   
-let myPizza = new Pizza("large", ["pepperoni", "salami", "olives", "onions", "ex. cheese"])
+let myPizza = new Pizza("large", ["pepperoni", "salami", "olives", "onions", "ex. cheese"], "deep dish")
 console.log(myPizza.sizeCost());
 console.log(myPizza.toppingCost());
+console.log(myPizza.styleCost());
 
 
  
